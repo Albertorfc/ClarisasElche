01f027bd46e72e98d449e8c493b485160f3453e6
Albertorfc <111996304+Albertorfc@users.noreply.github.com>
2025-07-13 23:23:18 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,20 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("imagenes");
-  eleventyConfig.addPassthroughCopy("favicon.png"); // 👈 Añade esta línea
+  eleventyConfig.addPassthroughCopy("favicon.png");
+
+  // 👇 AÑADIMOS LA COLECCIÓN DE NOTICIAS
+  eleventyConfig.addCollection("noticias", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("sections/noticias/*.md")
+      .filter(item => item.inputPath !== 'sections/noticias/index.md'); // excluye la portada
+  });
 
   return {
     dir: {
-      input: ".",
-      includes: "_includes",
+      input: ".",              // raíz del proyecto
+      includes: "_includes",   // includes y layouts
       layouts: "_includes/layouts",
-      output: "_site"
+      output: "_site"          // carpeta de salida de Netlify
     }
   };
 };
